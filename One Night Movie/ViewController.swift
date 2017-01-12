f24463816aae1adb2a4b4dbdaf1f6a756abb10de
Simon Chrétien <simon@macbook-pro-de-simon.home>
2017-01-12 13:44:41 +0100

    Initial Commit

---
+++ One Night Movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  One Night Movie
+//
+//  Created by Simon Chrétien on 12/01/2017.
+//  Copyright © 2017 Simon Chrétien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
